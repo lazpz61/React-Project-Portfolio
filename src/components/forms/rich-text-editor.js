ea66e6c5d7132020ebd86d22780ce3676a661ddd
LazaroMPerez <lazpz61@gmail.com>
2021-05-07 18:24:51 -0400

    rich text ediotr

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -22,6 +22,10 @@ export default class RichTextEditor extends Component {
         draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
     ))
    }
+
+   uploadFile(file) {
+       console.log("upload file", file);
+   }
    
 
    render() {
@@ -32,6 +36,18 @@ export default class RichTextEditor extends Component {
                wrapperClassName="demo-wrapper"
                editorClassname="demo-editor"
                onEditorStateChange={this.onEditorStateChange}
+               toolbar={{ 
+                    inline: { inDropdown: true},
+                    list: { inDropdown: true},
+                    textAlign: { inDropdown: true},
+                    history: { inDropdown: true},
+                    image: { 
+                        uploadCallback: this.uploadFile,
+                        alt: { present: true, mandatory: false},
+                        previewImage: true,
+                        inputAccept: "image/gif,image/jpeg,image/jpg,image/png,imgage/svg"
+                    }
+               }}
                />
            </div>
        )
